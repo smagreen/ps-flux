9912dc49882ad1aa6ddf221cbdeeece2325e37ef
Steve Green <home@steve-green.com>
2019-12-28 20:53:10 +0000

    Props State Lifecycle

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
+import CoursesPage from "./CoursesPage";
 
 const App = () => {
   const getPage = () => {
     const route = window.location.pathname;
     if (route === "/about") return <AboutPage />;
+    if (route === "/courses") return <CoursesPage />;
     return <HomePage />;
   };
 
