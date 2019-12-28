9d42bc828d2cef4db30e5f916188c8ba2319d599
Steve Green <home@steve-green.com>
2019-12-28 11:18:11 +0000

    End of Creating React Components

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="jumbotron">
+      <h1>Pluralsight Adminstration</h1>
+      <p>React, Flux, and React Router for ultra-responsive web apps.</p>
+      <a href="/about">About</a>
+    </div>
+  );
+};
+
+export default HomePage;
