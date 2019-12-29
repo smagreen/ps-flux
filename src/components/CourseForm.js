d27d8ea26d0b5ff93ee6062cfe09993f5d867183
Steve Green <home@steve-green.com>
2019-12-29 12:40:45 +0000

    Forms, Validation

---
+++ src/components/CourseForm.js
@@ -0,0 +1,58 @@
+import React from "react";
+import TextInput from "./common/TextInput";
+import PropTypes from "prop-types";
+
+function CourseForm(props) {
+  return (
+    <form onSubmit={props.onSubmit}>
+      <TextInput
+        id="title"
+        onChange={props.onChange}
+        name="title"
+        label="Title"
+        value={props.course.title}
+        error={props.errors.title}
+      />
+
+      <div className="form-group">
+        <label htmlFor="author">Author</label>
+        <div className="field">
+          <select
+            id="author"
+            name="authorId"
+            value={props.course.authorId || ""}
+            onChange={props.onChange}
+            className="form-control"
+          >
+            <option value="" />
+            <option value="1">Cory House</option>
+            <option value="2">Scott Allen</option>
+          </select>
+        </div>
+      </div>
+      {props.errors.authorId && (
+        <div className="alert alert-danger">{props.errors.authorId}</div>
+      )}
+
+      <TextInput
+        id="category"
+        onChange={props.onChange}
+        name="category"
+        label="Category"
+        value={props.course.category}
+        error={props.errors.category}
+      />
+
+      <input type="submit" value="Save" className="btn btn-primary" />
+    </form>
+  );
+}
+
+CourseForm.propTypes = {
+  course: PropTypes.object.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  error: PropTypes.object.isRequired
+};
+
+export default CourseForm;
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,12 +1,65 @@
-import React from "react";
-// import { Prompt } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
+import { toast } from "react-toastify";
+
+// import { Link } from "react-router-dom";
 
 const ManageCoursePage = props => {
+  const [errors, setErrors] = useState({});
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: ""
+  });
+
+  useEffect(() => {
+    const slug = props.match.params.slug;
+    if (slug) {
+      courseApi.getCourseBySlug(slug).then(_course => setCourse(_course));
+    }
+  }, [props.match.params.slug]);
+
+  const handleChange = event => {
+    setCourse({
+      ...course,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const formIsValid = event => {
+    const _errors = {};
+
+    if (!course.title) _errors.title = "Title is required";
+    if (!course.authorId) _errors.authorId = "Author is required";
+    if (!course.category) _errors.category = "Category is required";
+
+    setErrors(_errors);
+    return Object.keys(_errors).length === 0;
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (!formIsValid()) return;
+
+    courseApi.saveCourse(course).then(() => {
+      props.history.push("/courses");
+      toast.success("Course Saved.");
+    });
+  };
+
   return (
     <>
       <h2>Manage Course</h2>
       {/* <Prompt when={true} message="Are you sure you want to leave?" /> */}
-      <p>{props.match.params.slug}</p>
+      <CourseForm
+        errors={errors}
+        course={course}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     </>
   );
 };
